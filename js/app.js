cda4991741d4722fc937cb0a759f3a365319870c
boror <bororadojcic1991@gmail.com>
2021-10-15 13:00:27 +0100

    project is done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
+const container = document.querySelector(".container");
+
+const divResult = document.createElement("div");
+divResult.classList.add("result");
+
+const playerResult = document.createElement('p');
+playerResult.classList.add('playerResult');
+playerResult.textContent = 0;
+
+const computerResult = document.createElement('p');
+computerResult.classList.add('computerResult');
+computerResult.textContent = 0;
+
+divResult.appendChild(playerResult);
+divResult.appendChild(computerResult);
+
+const msg = document.createElement('p');
+msg.classList.add('msg');
+msg.textContent = '';
+
+const res = document.createElement('p');
+res.classList.add('res');
+res.textContent = 'Result';
+
+container.appendChild(res);
+container.appendChild(divResult);
+container.appendChild(msg);
+
 function computerPlay(){
     const selection  = ["ROCK", "PAPER", "SCISSORS"];
     const random = Math.floor(Math.random() * selection.length);
@@ -15,12 +43,10 @@ function playRound(playerSelection, computerSelection) {
 
 let playerWin = 0;
 let computerWin = 0;
-function game(){
+function game(playerSelection){
     let msg;
-    let playerSelection = prompt("Please enter your name");
-
     let result = playRound(playerSelection, computerPlay());
-    // console.log(result);
+
     if(result === 0){ 
         msg = "It's tie!"
     } else if(result === 1){ 
@@ -34,16 +60,27 @@ function game(){
     return [msg, playerWin, computerWin];
 }
 
-for (let index = 0; index < 20; index++) {
-    let result = game();
-    console.log(result[0]+" Results: "+result[1]+" "+result[2]);
-    if(result[1] === 5){
-        console.log("Player win the match");
-        break;
-    }else if(result[2] === 5){ 
-        console.log("Computer win the match");
-        break; 
-    }else{
-        continue;
-    }
-}
+const btns = document.querySelectorAll('button');
+
+btns.forEach(btn => {
+   btn.addEventListener('click', event => {
+        let result = game(event.target.id);
+        // console.log(result[0]+" Results: "+result[1]+" "+result[2]);
+        playerResult.textContent = result[1];
+        computerResult.textContent = result[2];
+        msg.textContent = result[0];
+        if(result[1] === 5){
+            // console.log("Player win the match");
+            msg.textContent = "Player win the match";
+            document.getElementById("rock").disabled = true;
+            document.getElementById("paper").disabled = true;
+            document.getElementById("scissors").disabled = true;
+        }else if(result[2] === 5){ 
+            // console.log("Computer win the match");
+            msg.textContent = "Computer win the match";
+            document.getElementById("rock").disabled = true;
+            document.getElementById("paper").disabled = true;
+            document.getElementById("scissors").disabled = true;
+        }
+   });
+});
